844651995c011f9f04aece5066fa0b384fb183cd
Rahees9983 <khanrahees333@gmail.com>
2020-12-07 15:21:02 +0530

    aa

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET','POST'])
+def insert_data():
+    return "This is a post url "    
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000,host='0.0.0.0')
